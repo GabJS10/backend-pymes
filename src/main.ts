db13f901fd53f031745137e5bb966de79e115949
GabJS10 <jgabis65@gmail.com>
2025-01-14 18:46:52 -0500

    railway 1.4

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     origin: 'http://localhost:3000',
     credentials: true,
   })
